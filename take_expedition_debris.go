eb95e5e52d21d1cf85e05271c4d3c7c1056f235c
RockClubKASHMIR <44343689+RockClubKASHMIR@users.noreply.github.com>
2019-12-29 15:51:46 +0200

    Update take_expedition_debris.go

--- take_expedition_debris.go
+++ take_expedition_debris.go
@@ -3,7 +3,7 @@
 //--- WARNING!!! This script can work ONLY if you are Discoverer! ---
 fromSystem = 1 // Your can change this value as you want
 toSystem = 200 // Your can change this value as you want
-Pnbr = 1  // When Rnbr = 1, the script will search debris for minimum 2 Pathfinders. You can set this value from 0, to the number you want
+Pnbr = 1  // When Pnbr = 1, the script will search debris for minimum 2 Pathfinders. You can set this value from 0, to the number you want
 times = 1 // if times = 1, the script will full scan 2 times the galaxy, from system, to system you want. You can set this value from 0, to the number you want
 //----
 cycle = 0
@@ -12,8 +12,10 @@ origin = nil
 flts = 0
 nbr = 0
 err = nil
+if (Pnbr < 0) {Pnbr = 0}
+if (times < 0) {times = 0}
 totalSlots = GetSlots().Total - GetFleetSlotsReserved()
-// Start to Search highest amount of Pathfinders on all your Planets and Moons(if you have some)
+// Start to Search highest amount of Recyclers on all your Planets and Moons(if you have some)
 for celestial in GetCachedCelestials() {
     ships, _ = celestial.GetShips()
     if ships.Pathfinder > flts {
@@ -23,31 +25,33 @@ for celestial in GetCachedCelestials() {
 }
 if origin != nil {
     Print("Your origin is "+origin.Coordinate)
+    if toSystem > 499 || toSystem == 0 {toSystem = -1}
+    if fromSystem > toSystem {Print("Please, type correctly fromSystem and/or toSystem!")}
     for system = curSystem; system <= toSystem; system++ {
-        systemInfo, _ = GalaxyInfos(origin.GetCoordinate().Galaxy, system)
+        systemInfos, b = GalaxyInfos(origin.GetCoordinate().Galaxy, system)
         Dtarget, _ = ParseCoord(origin.GetCoordinate().Galaxy+":"+system+":"+16)
         Sleep(Random(500, 1500)) // for avoid ban
         slots = GetSlots().InUse
         if err != nil {slots = totalSlots}
         if slots < totalSlots {
-            if Dtarget != nil {
+            if b == nil {
                 Print("Checking "+Dtarget)
-                if systemInfo.ExpeditionDebris.PathfindersNeeded > Pnbr { 
+                if systemInfos.ExpeditionDebris.PathfindersNeeded > Pnbr { 
                     ships, _ = origin.GetShips()
-                    if systemInfo.ExpeditionDebris.Metal == 0 && systemInfo.ExpeditionDebris.Crystal > 0 {Print("Found Crystal: "+systemInfo.ExpeditionDebris.Crystal)}
-                    if systemInfo.ExpeditionDebris.Metal > 0 && systemInfo.ExpeditionDebris.Crystal == 0 {Print("Found Metal: "+systemInfo.ExpeditionDebris.Metal)}
-                    if systemInfo.ExpeditionDebris.Metal > 0 && systemInfo.ExpeditionDebris.Crystal > 0 {Print("Found Metal: "+systemInfo.ExpeditionDebris.Metal+" and Crystal: "+systemInfo.ExpeditionDebris.Crystal)}
+                    if systemInfos.ExpeditionDebris.Metal == 0 && systemInfos.ExpeditionDebris.Crystal > 0 {Print("Found Crystal: "+systemInfos.ExpeditionDebris.Crystal)}
+                    if systemInfos.ExpeditionDebris.Metal > 0 && systemInfos.ExpeditionDebris.Crystal == 0 {Print("Found Metal: "+systemInfos.ExpeditionDebris.Metal)}
+                    if systemInfos.ExpeditionDebris.Metal > 0 && systemInfos.ExpeditionDebris.Crystal > 0 {Print("Found Metal: "+systemInfos.ExpeditionDebris.Metal+" and Crystal: "+systemInfos.ExpeditionDebris.Crystal)}
                     f = NewFleet()
                     f.SetOrigin(origin)
                     f.SetDestination(Dtarget)
                     f.SetSpeed(HUNDRED_PERCENT)
                     f.SetMission(RECYCLEDEBRISFIELD)
-                    nbr = systemInfo.ExpeditionDebris.PathfindersNeeded
-                    if systemInfo.ExpeditionDebris.PathfindersNeeded > ships.Pathfinder {nbr = ships.Pathfinder}
+                    nbr = systemInfos.ExpeditionDebris.PathfindersNeeded
+                    if systemInfos.ExpeditionDebris.PathfindersNeeded > ships.Pathfinder {nbr = ships.Pathfinder}
                     f.AddShips(PATHFINDER, nbr)
                     a, err = f.SendNow()
                     if err == nil {
-                        if nbr < systemInfo.ExpeditionDebris.PathfindersNeeded {Print("You don't have enough Ships for this debris field!")}
+                        if nbr < systemInfos.ExpeditionDebris.PathfindersNeeded {Print("You don't have enough Ships for this debris field!")}
                         if nbr > 1 {
                             Print(nbr+" Pathfinders are sended successfully!")
                         } else {Print(nbr+" Pathfinder is sended successfully!")}
@@ -66,36 +70,41 @@ if origin != nil {
             for slots == totalSlots {
                 if err != 0 {
                     Print("Please wait till ships lands! Recheck after "+ShortDur(2*60))
-                    Sleep(120000)
+                    Sleep(2*60*1000)
                     ships, _ = origin.GetShips()
                     if ships.Pathfinder > 0 {slots = GetSlots().InUse}
                     err = nil
                 } else {
                     Print("All Fleet slots are busy now! Please, wait "+ShortDur(2*60))
-                    Sleep(120000)
+                    Sleep(2*60*1000)
                     slots = GetSlots().InUse
                 }
                 curSystem = system-1
             }
         }
-        if system >= toSystem {
-            if times > 0 {
-                if cycle < times {
-                    cycle++
-                    if nbr == 0 {Print("Not found any debris! Start searching again...")}
-                    curSystem = fromSystem-1
-                    system = curSystem
-                    Sleep(4000)
+        if b == nil {
+            if system >= toSystem {
+                if times > 0 {
+                    if cycle < times {
+                        cycle++
+                        if nbr == 0 {Print("Not found any debris! Start searching again...")}
+                        curSystem = fromSystem-1
+                        system = curSystem
+                        Sleep(4000)
+                    } else {
+                        Print("You made "+(times+1)+" times full scan all systems chosen by you! The script turns off")
+                        SendTelegram(TELEGRAM_CHAT_ID, "You made "+(times+1)+" times full scan all systems chosen by you! The script turns off")
+                        break
+                    }
                 } else {
-                    Print("You made "+(times+1)+" times full scan all systems chosen by you! The script turns off")
-                    SendTelegram(TELEGRAM_CHAT_ID, "You made "+(times+1)+" times full scan all systems chosen by you! The script turns off")
+                    Print("You made full scan all systems chosen by you! The script turns off")
+                    SendTelegram(TELEGRAM_CHAT_ID, "You made full scan all systems chosen by you! The script turns off")
                     break
                 }
-            } else {
-                Print("You made full scan all systems chosen by you! The script turns off")
-                SendTelegram(TELEGRAM_CHAT_ID, "You made full scan all systems chosen by you! The script turns off")
-                break
             }
+        } else {
+            Print("Please, type correctly fromSystem and/or toSystem!")
+            break
         }
     }
 } else {Print("You don't have Pathfinders on your Planets/Moons!")}
