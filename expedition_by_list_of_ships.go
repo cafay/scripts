585d304d352a1bb119c18cab08240069302f75f0
RockClubKASHMIR <44343689+RockClubKASHMIR@users.noreply.github.com>
2020-04-09 23:17:43 +0300

    Update expedition_by_list_of_ships.go

--- expedition_by_list_of_ships.go
+++ expedition_by_list_of_ships.go
@@ -1,6 +1,6 @@
 /***** This script is created by RockClubKASHMIR <discord @RockClubKASHMIR#8058> *****\
  
- v4.2-1
+ v4.32
  
     DESCRIPTION
  1. The script can send fleets from more than 1 planet/moon
@@ -13,22 +13,21 @@
  5. Evenly distribution of EXPO slots per each moon/planet (can be turn on/of)
 */
 
-homes = ["M:1:2:3"] /*Replace M:1:2:3 with your coordinate - M for the moon, P for planet.
-        You can add as many planets/moons you want - the home list must look like this: homes = ["M:1:2:3", "M:2:2:3"]
-*/
+homes = ["M:1:2:3"] // Replace M:1:2:3 with your coordinate - M for the moon, P for planet.
+// You can add as many planets/moons you want - the home list must look like this: homes = ["M:1:2:3", "M:2:2:3"]
+
 shipsList = {LARGECARGO: 0, LIGHTFIGHTER: 0, PATHFINDER: 1}// Set your Ships list
 
-splitSlots = true // Do you want evenly distribution of EXPO slots per each moon/planet? true = YES / false = NO
-sendAtOnce = false // Do you want to send the ships set with quantity 0 at once? true = YES / false = NO
+splitSlots = true //Do you want evenly distribution of EXPO slots per each moon/planet? true = YES / false = NO
+sendAtOnce = false //Do you want to send the ships set with quantity 0 at once? true = YES / false = NO
 
-minusCurrentSystem = 5 // Set this as start destination of range coordinates - minus your solar system
-plusCurrentSystem = 5 // Set this as end destination of range coordinates - plus your solar system
+minusCurrentSystem = 5 // Set this as start destination of range coordinates - minus your current world's system
+plusCurrentSystem = 5 // Set this as end destination of range coordinates - plus your current world's system
 
 DurationOfExpedition = 1 // Set duration (in hours) of the EXPEDITION: minimum 1 - maximum 8
 PathfindersDebris = true // Do you want to get EXPO debrises? true = YES / false = NO
 Pnbr = 5  // The script will ignore debris less than for PATHFINDERS that you set - The Maximum PATHFINDERS is limited only of your PATHFINDERS on the current moon/planet! You can set this value from 1, to the number you want
 PathfinderSystemsRange = true // Do you want to check/get EXPO debris in range systems? true = YES / false = NO
-
 SystemsRange = false // Do you want to send your EXPO fleet to Range coordinates? true = YES / false = NO
 Repeat = true // Do you want to repeat the full cycle of fleet sending? true = YES / false = NO
 HowManyCycles = 5 // Set the limit of repeats of whole cycle of EXPO fleet sending - 0 means forewer
